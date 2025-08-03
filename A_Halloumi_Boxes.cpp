b79c38825093aa75837e1fc7197caac325a4bcff
Ayushnotafk <ayushthakur2240@gmail.com>
2025-08-03 18:04:43 +0530

    Add files via upload

---
+++ A_Halloumi_Boxes.cpp
@@ -0,0 +1,110 @@
+// Author      : Ayush Thakur
+// Institute   : IIITDM Jabalpur
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define all(x) x.begin(), x.end()
+#define yes cout << "Yes\n"
+#define no cout << "No\n"
+#define endl '\n'
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+// GCD and LCM
+ll gcd(ll a, ll b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    return (a / gcd(a, b)) * b;
+}
+
+// Input vector
+template<typename T>
+void input_vector(vector<T> &v) {
+    for (auto &x : v) cin >> x;
+}
+
+// Print a vector
+void printVec(const vector<int>& v) {
+    for (auto val : v) cout << val << ' ';
+    cout << '\n';
+}
+// Debug Print Helpers
+void _print(int x) { cerr << x; }
+void _print(long x) { cerr << x; }
+void _print(long long x) { cerr << x; }
+void _print(unsigned x) { cerr << x; }
+void _print(unsigned long x) { cerr << x; }
+void _print(unsigned long long x) { cerr << x; }
+void _print(float x) { cerr << x; }
+void _print(double x) { cerr << x; }
+void _print(long double x) { cerr << x; }
+void _print(char x) { cerr << '\'' << x << '\''; }
+void _print(const char* x) { cerr << '"' << x << '"'; }
+void _print(const string& x) { cerr << '"' << x << '"'; }
+void _print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V> void _print(const pair<T, V>& x) {
+    cerr << '{'; _print(x.first); cerr << ", "; _print(x.second); cerr << '}';
+}
+template <typename T> void _print(const vector<T>& v) {
+    cerr << '[';
+    for (size_t i = 0; i < v.size(); ++i) { _print(v[i]); if (i != v.size() - 1) cerr << ", "; }
+    cerr << ']';
+}
+template <typename T> void _print(const set<T>& v) {
+    cerr << '{'; for (auto i : v) { _print(i); cerr << ", "; } cerr << '}';
+}
+template <typename T, typename V> void _print(const map<T, V>& v) {
+    cerr << '{'; for (auto i : v) { _print(i); cerr << ", "; } cerr << '}';
+}
+bool sorted(vi &a){
+        for (int i = 0; i < a.size()-1; i++) {
+            if(a[i]>a[i+1]){
+                return false;
+            }
+            
+        }
+        return true;
+    }
+
+void solve() {
+int n,k;
+ cin >> n>>k;
+    vi a(n);
+    input_vector(a);
+    debug(a);
+    if(k!=1||sorted(a)){
+       yes;
+    }
+    else{
+        no;
+    }
+}
+
+int main() {
+    fast_io
+    int t ;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ A_In_Search_of_an_Easy_Problem.cpp
@@ -0,0 +1,104 @@
+// Author      : Ayush Thakur
+// Institute   : IIITDM Jabalpur
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fast_io ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define ll long long
+#define ull unsigned long long
+#define pb push_back
+#define vi vector<int>
+#define vll vector<ll>
+#define pii pair<int, int>
+#define all(x) x.begin(), x.end()
+#define yes cout << "Yes\n"
+#define no cout << "No\n"
+#define endl '\n'
+
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x << " = "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+
+// GCD and LCM
+ll gcd(ll a, ll b) {
+    while (b) {
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
+
+ll lcm(ll a, ll b) {
+    return (a / gcd(a, b)) * b;
+}
+
+// Input vector
+template<typename T>
+void input_vector(vector<T> &v) {
+    for (auto &x : v) cin >> x;
+}
+
+// Print a vector
+void printVec(const vector<int>& v) {
+    for (auto val : v) cout << val << ' ';
+    cout << '\n';
+}
+// Debug Print Helpers
+void _print(int x) { cerr << x; }
+void _print(long x) { cerr << x; }
+void _print(long long x) { cerr << x; }
+void _print(unsigned x) { cerr << x; }
+void _print(unsigned long x) { cerr << x; }
+void _print(unsigned long long x) { cerr << x; }
+void _print(float x) { cerr << x; }
+void _print(double x) { cerr << x; }
+void _print(long double x) { cerr << x; }
+void _print(char x) { cerr << '\'' << x << '\''; }
+void _print(const char* x) { cerr << '"' << x << '"'; }
+void _print(const string& x) { cerr << '"' << x << '"'; }
+void _print(bool x) { cerr << (x ? "true" : "false"); }
+
+template <typename T, typename V> void _print(const pair<T, V>& x) {
+    cerr << '{'; _print(x.first); cerr << ", "; _print(x.second); cerr << '}';
+}
+template <typename T> void _print(const vector<T>& v) {
+    cerr << '[';
+    for (size_t i = 0; i < v.size(); ++i) { _print(v[i]); if (i != v.size() - 1) cerr << ", "; }
+    cerr << ']';
+}
+template <typename T> void _print(const set<T>& v) {
+    cerr << '{'; for (auto i : v) { _print(i); cerr << ", "; } cerr << '}';
+}
+template <typename T, typename V> void _print(const map<T, V>& v) {
+    cerr << '{'; for (auto i : v) { _print(i); cerr << ", "; } cerr << '}';
+}
+
+void solve() {
+    int n;
+    cin >> n;
+    vi a(n);
+    input_vector(a);
+    int co=0;
+    for (int i = 0; i < n; i++) {
+        if(a[i]==1){co ++;}
+    }
+    if(co>0){
+        cout<<"HARD";
+    }
+    else{
+        cout<<"EASY";
+    }
+}
+
+int main() {
+    fast_io
+    int t=1;
+    // cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
